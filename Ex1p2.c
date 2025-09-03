91f99df8a3b9d6d7eb6ebbe3da2708802297ed9b
Eduardo G <eduardogomes5577@gmail.com>
2025-09-03 14:42:31 -0300

    Lista de String

--- Ex1p2.c
+++ Ex1p2.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-int strcopy (char s1[],char s2[]);
+int strcontacatenacao (char s1[],char s2[]);
 
 void main ()
 {
     char s1[20]="aleatorio";
     char s2[8]= "copiar";
-    if(strcopy(s1,s2)==1)
+    if(strcontacatenacao(s1,s2)==1)
     {
     printf("Copiado com sucesso.");
     }
     else
     {
-        printf("Nao foi copiado. %s",strcat(s1,s2));
+        printf("Nao foi copiado.");
     }
 }
-int strcopy (char s1[],char s2[])
+int strcontacatenacao (char s1[],char s2[])
 {
     int i,j=strlen(s1);
 
