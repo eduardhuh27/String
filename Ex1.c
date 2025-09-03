361d7a2534791e3b28bc59149c2d516a8da4698c
Eduardo G <eduardogomes5577@gmail.com>
2025-09-03 14:18:30 -0300

    Lista de String

---
+++ Ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int strlenC (char s1[]);
+
+void main ()
+{
+    char s1[20]="Aleatorio";
+    printf("%d",strlenC (s1));
+}
+int strlenC (char *s1[])
+{
+    int i,cont=0;
+
+    for (i=1;s1!=0;i++)
+    {
+
+        cont++;
+    }
+    return cont;
+
+
+}
